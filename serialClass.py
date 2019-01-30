60396e35b1eea25f2b391e4784cb8608fa460c68
bingyinh <bingyin.hu@gmail.com>
2019-01-30 14:46:27 -0500

    added a function to return interpolated/extrapolated value at a given X

--- serialClass.py
+++ serialClass.py
@@ -36,3 +36,41 @@ class serial(object):
     def intp(self, x0, x1, y1, x2, y2):
         # print('x0:%f, x1:%f, y1:%f, x2:%f, y2:%f' %(x0,x1,y1,x2,y2))
         return y1-(y2-y1)*1.0/(x2-x1)*(x1-x0)
+
+    # returns y corresponding to the given x, may need interpolation
+    def yAtX(self, givenX, sorting = 'on'):
+        y = None # init y
+        if sorting != 'off':
+            # sort the serial based on X
+            [self._Xins, self._Yins] = list(zip(*sorted(zip(self._Xins, self._Yins))))
+        # case 1: X smaller than the smallest Xins
+        if givenX < self._Xins[0]:
+            y = self.intp(givenX, self._Xins[0], self._Yins[0],
+                                  self._Xins[1], self._Yins[1])
+        # case 2: X larger than the largest Xins
+        elif givenX >= self._Xins[-1]:
+            y = self.intp(givenX, self._Xins[-2], self._Yins[-2],
+                                  self._Xins[-1], self._Yins[-1])
+        # general case: X is within the range of Xins
+        else:
+            leftIndex = 0 # init leftIndex
+            for i in xrange(len(self._Xins)):
+                if self._Xins[i] > givenX:
+                    leftIndex = i - 1
+                    break
+            rightIndex = leftIndex + 1
+            y = self.intp(givenX, self._Xins[leftIndex], self._Yins[leftIndex],
+                                  self._Xins[rightIndex], self._Yins[rightIndex])
+        return y
+
+# Test section
+if __name__ == '__main__':
+    X1 = [1,2,3,4]
+    Y1 = [1,2,3,4]
+    tag1 = 'tag1'
+    s1 = serial(X1, Y1, tag1)
+    assert(s1.yAtX(0) == 0)
+    assert(s1.yAtX(1) == 1)
+    assert(s1.yAtX(2.5) == 2.5)
+    assert(s1.yAtX(4) == 4)
+    assert(s1.yAtX(5) == 5)

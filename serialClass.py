22b0de8ad5adedee1eef4387ed5da5ba7623001d
bingyinh <bingyin.hu@gmail.com>
2022-05-02 22:08:36 -0400

    add support for log scale interpolation

--- serialClass.py
+++ serialClass.py
@@ -1,7 +1,9 @@
 # serial object class
 # By Bingyin Hu
+import numpy as np
+
 class serial(object):
-    def __init__(self, Xins, Yins, tag):
+    def __init__(self, Xins, Yins, tag, logX=False, logY=False):
         # check length of Xins, Yins
         if len(Xins) < 2:
             raise ValueError("The length of input X array must be greater than 2.")
@@ -24,6 +26,8 @@ class serial(object):
         self.outPter = 0 # pointer to Xouts, Youts
         self.end = len(self._Xins) # raise the end flag when in pointer >= end
         self.flag = False
+        self.logX = logX
+        self.logY = logY
     @property
     def Xins(self):
         return self._Xins
@@ -35,7 +39,16 @@ class serial(object):
     # x1, y1, x2, y2: coordinates of the known points
     def intp(self, x0, x1, y1, x2, y2):
         # print('x0:%f, x1:%f, y1:%f, x2:%f, y2:%f' %(x0,x1,y1,x2,y2))
-        return y1-(y2-y1)*1.0/(x2-x1)*(x1-x0)
+        if self.logX:
+            x0 = np.log10(x0)
+            x1 = np.log10(x1)
+            x2 = np.log10(x2)
+        if self.logY:
+            y1 = np.log10(y1)
+            y2 = np.log10(y2)
+            return 10**(y1-(y2-y1)*1.0/(x2-x1)*(x1-x0))
+        else:
+            return y1-(y2-y1)*1.0/(x2-x1)*(x1-x0)
 
     # returns y corresponding to the given x, may need interpolation
     def yAtX(self, givenX, sorting = 'on'):
@@ -54,7 +67,7 @@ class serial(object):
         # general case: X is within the range of Xins
         else:
             leftIndex = 0 # init leftIndex
-            for i in xrange(len(self._Xins)):
+            for i in range(len(self._Xins)):
                 if self._Xins[i] > givenX:
                     leftIndex = i - 1
                     break
